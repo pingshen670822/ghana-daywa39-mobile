938d337a684144eb29b57f843f3059a109138d76
Ghana39 Auto Publish <ghana39-auto-publish@users.noreply.github.com>
2026-07-17 11:55:58 +0800

    Update Ghana39 report 20260717073149

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+
+const CACHE_NAME = 'ghana39-mobile-20260717073149';
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'CLEAR_CACHE') {
+    event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
+  }
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request, { cache: 'no-store' }).catch(() => caches.match(event.request)));
+});
